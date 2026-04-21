8f884c8965074ec2c63d55d42d95249f89f829ca
Joan Ferley Mosquera Lozano <mosqueralozanojhoanferley@gmail.com>
2026-04-20 21:53:40 -0500

    Initial Setup

---
+++ src/main/java/com/dunnwr/saml_spring_demo/SamlSpringDemoApplication.java
@@ -0,0 +1,13 @@
+package com.dunnwr.saml_spring_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SamlSpringDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SamlSpringDemoApplication.class, args);
+	}
+
+}
